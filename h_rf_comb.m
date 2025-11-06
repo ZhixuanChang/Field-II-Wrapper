16fe6e05bb7c5cd1c49fc459006d42d7de6016c7
Chang Zhixuan <chang_zhixuan@outlook.com>
2025-11-06 15:38:50 +0800

    [ADD] add h_rf_comb which can combine scattering signal set into a single matrix.

---
+++ h_rf_comb.m
@@ -0,0 +1,34 @@
+function rfdata = h_rf_comb(rf_data_set, t_start_set, fs)
+% Align RF data of multiple emission cycles.
+%
+% The calculated RF signals of Field is unaligned. To obtain the spatial spectrum, the signal must be aligned first.
+%
+% Parameters
+% rf_data_set - a cell contains all scattering signals.
+% t_start_set - the start time vector.
+% fs - the sampling rate.
+%
+% Example
+% rfdata = h_rf_comb(rf_data_set, t_start_set, fs);
+%
+% See also
+% calc_scat_all, calc_scat_multi
+
+num_emit = length(t_start_set);
+num_elem = size(rf_data_set{1}, 2);
+
+num_padd = round(t_start_set * fs);
+num_samp = zeros(1, num_emit);
+for i = 1 : num_emit
+    num_samp(i) = num_samp(i) + size(rf_data_set{i}, 1);
+end
+
+num_samp = num_samp + num_padd;
+num_samp = max(num_samp, [], 'all');
+
+rfdata = zeros(num_samp, num_elem, num_emit);
+for i = 1 : num_emit
+    rfdata(num_padd(i)+1 : num_padd(i)+size(rf_data_set{i},1), :, i) = rf_data_set{i};
+end
+
+end
